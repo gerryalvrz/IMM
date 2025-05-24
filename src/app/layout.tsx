98963391c95b9e0718fcc6f0d053eaa8c13c01c7
cesar angulo <cesarangulo@cesars-Mac-Studio.local>
2025-05-24 13:56:14 -0700

    linting issues for deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,7 @@ export default function RootLayout({
         />
       </head>
       <body suppressHydrationWarning>
+        <WalletProvider>
         <MetaMaskProvider>
         <TokenFactoryProvider>
           <QueryProvider>
@@ -65,6 +66,7 @@ export default function RootLayout({
           </QueryProvider>
           </TokenFactoryProvider>
         </MetaMaskProvider>
+        </WalletProvider>
       </body>
     </html>
   );
